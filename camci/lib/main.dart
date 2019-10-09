2be60887f8dc8220e8b8f2815498f18c65550cfc
jorge <coqueju97@gmail.com>
2019-10-09 16:55:53 -0500

    jajajaja

--- camci/lib/main.dart
+++ camci/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:camci/autenticacion/singin.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,11 +8,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home: ,
+      home:  Signin(),
     );
   }
 }
